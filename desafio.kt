24c2457fb04cce225b823be31ee42179bc0dba5c
gabrielmota00 <77344280+gabrielmota00@users.noreply.github.com>
2023-12-16 11:31:30 -0300

    Teste

--- desafio.kt
+++ desafio.kt
@@ -2,20 +2,23 @@
 
 enum class Nivel { INICIANTE, INTERMEDIARIO, AVANÇADO }
 
-data class Aluno(val nome: String)
+data class Aluno(val nome: String, var age: Int){
+    var matriculas = matricular
+}
 
 data class ConteudoEducacional(val nome: String, var duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
 
-    val inscritos = mutableListOf<Usuario>()
+    val inscritos = mutableListOf<Aluno>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(usuario: Aluno) {
+        // Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).
+
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.
+    // Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.
 }
